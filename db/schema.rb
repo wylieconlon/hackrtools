d001c5cbe54ae52af6c7d09bea1cb18aa913439b
Ali Ukani <ali.ukani@gmail.com>
2012-03-03 14:52:34 -0500

    Snippets.description is now Snippets.title

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303191755) do
+ActiveRecord::Schema.define(:version => 20120303195121) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(:version => 20120303191755) do
   end
 
   create_table "snippets", :force => true do |t|
-    t.text     "description"
+    t.text     "title"
     t.string   "link"
     t.text     "code"
-    t.boolean  "public",      :default => true
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "public",     :default => true
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
   end
 
 end
