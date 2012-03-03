81dd32f633b94d74e29e70fa5c3f32bccfccc049
Ali Ukani <ali.ukani@gmail.com>
2012-03-03 16:39:22 -0500

    Added tags.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303195121) do
+ActiveRecord::Schema.define(:version => 20120303213625) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(:version => 20120303195121) do
     t.boolean  "public",     :default => true
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
