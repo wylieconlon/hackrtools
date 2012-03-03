d7abd30170ef5e0f86b95966f4e30cef74775545
Ali Ukani <ali.ukani@gmail.com>
2012-03-03 16:09:54 -0500

    Snippet descriptions are totally now titles, dawg.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,5 @@
 class Snippet < ActiveRecord::Base
-  validates_presence_of :description, :code
+  validates_presence_of :title, :code
   validates :description, :length => { :maximum => 140 }
 
 end
