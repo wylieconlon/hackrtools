004c7fbbf1e2111aa71bc9cbc0b0f688653fca17
Wylie Conlon <wylieconlon@gmail.com>
2012-03-04 08:28:48 -0500

    Fixed controller inheritance

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,19 @@
 class ArticlesController < ItemsController
-
+  # POST /items
+  # POST /items.json
+  def create
+    puts "Saving article: #{params}"
+    
+    @item = Item.new(params[:item])
+    
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.json { render json: @item, status: :created, location: @item }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
