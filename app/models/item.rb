c8bda58e8b5f9bb2131b9bbe3ef79bb184f35d4c
Ali Ukani <ali.ukani@gmail.com>
2012-03-04 00:36:05 -0500

    Try to make Item aware of its subclasses. http://code.alexreisner.com/articles/single-table-inheritance-in-rails.html

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ActiveRecord::Base
+
+  def self.inherited(child)
+    child.instance_eval do
+      def model_name
+        Item.model_name
+      end
+    end
+    super
+  end
+
+  def self.select_options
+    descendants.map{ |c| c.to_s }.sort
+  end
+
 end
