5e5cf8af0a039d9a8a9233fbd9b8ee2f5179495f
Ali Ukani <ali.ukani@gmail.com>
2012-03-03 23:07:48 -0500

    Added Code column to Items.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120304034714) do
+ActiveRecord::Schema.define(:version => 20120304040626) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120304034714) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "type"
+    t.text     "code"
   end
 
   create_table "snippets", :force => true do |t|
