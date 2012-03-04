92be462a12ac8651ab241b1c6e99277b9ae24ec8
Eric Kelly <me@useric.com>
2012-03-04 06:52:04 -0500

    Fixed ajax url

--- public/js/saveDialog.js
+++ public/js/saveDialog.js
@@ -67,7 +67,7 @@ hckrtools.saveDialog = {
 		    snip = options.snip || null,
 		    that = hckrtools.saveDialog;
 		
-		var request = "http://hckrtools.com/" + type + "/new"
+		var request = "http://hackrtools.com/" + type + "/new"
 
 		var option = {
 			title: document.title,
