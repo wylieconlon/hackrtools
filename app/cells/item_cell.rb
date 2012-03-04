e79b112761ecf71a5772ff187886833cad7da048
Wylie Conlon <wylieconlon@gmail.com>
2012-03-04 14:02:31 -0500

    Added tags etc

--- app/cells/item_cell.rb
+++ app/cells/item_cell.rb
@@ -1,8 +1,12 @@
+require 'csv'
+
 class ItemCell < Cell::Rails
   def show(args)
     @item = args[:item]
     @hasCode = defined?(@item.code)
     # @timeAgo = time_ago_in_words(args[:item][:updated_at])
+    
+    @tags = @item.tags
     
     render
   end
