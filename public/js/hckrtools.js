03eca92b4be99856e2d6709656c490c20455bfad
Eric Kelly <me@useric.com>
2012-03-03 23:14:48 -0500

    Bookmarklet added

---
+++ public/js/hckrtools.js
@@ -0,0 +1,47 @@
+hckrtools = {
+	isolateElements: function($) {
+		var snippets = $('pre, code:not(pre :first-child), .syntaxHighlight');
+		$.each(snippets, function(i, val) {
+			if($(this).css('display') == 'block') {
+				var content = hckrtools.flatten($, $(this))
+				$(this).wrap('<div class="hckrsnippet" style="position:relative"><pre class="highlight" /></div>')
+				var $parent = $('div.hckrsnippet').last()
+				$(this).parent().html(content)
+			}
+		});
+		$('pre.highlight').syntaxHighlight()
+	},
+	createControls: function($) {
+		var snippets = $('div.hckrsnippet'),
+		    controls = "<span class='copy'>Copy</span><span class='save'>Save</span><span class='Edit'>Edit</span>";
+
+		$.each(snippets, function(i, val) {
+			$(this).prepend(controls);
+		});
+		$('div.hckrsnippet > span').css('float', 'right');
+	},
+	flatten: function($, $el) {
+		var arr = $el.html().split(/<\s*br\s*\/?\s*>/i)
+		for(var i = 0; i < arr.length; i++) {
+			arr[i] = arr[i].replace(/(<([^>]+)>)/ig,"")
+		}
+		return arr.join('\n') 
+	},
+	load: function() {
+		var sh=document.createElement('script')
+		sh.setAttribute("type","text/javascript")
+		sh.setAttribute("src", "http://hackrtools.com/jquery.syntaxhighlighter.min.js")
+		document.getElementsByTagName("head")[0].appendChild(sh)
+	},
+	init: function($) {
+		var that = hckrtools;
+		$.SyntaxHighlighter.init({lineNumbers:true,stripEmptyStartFinishLines:true});
+		that.isolateElements($);
+		that.createControls($);
+
+		if(jQueryOld != undefined) {
+			$ = jQueryOld.noConflict(true);
+		}
+	}
+}
+
