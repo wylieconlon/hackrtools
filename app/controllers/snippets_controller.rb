b87c1b5893859f0a3d8ae9c9c9d45bc79ee91219
Wylie Conlon <wylieconlon@gmail.com>
2012-03-04 20:29:44 -0500

    Updated routes for demo

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -11,7 +11,7 @@ class SnippetsController < ItemsController
     
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Item was successfully created.' }
         format.json { render json: @item, status: :created, location: @item }
       else
         format.html { render action: "new" }
